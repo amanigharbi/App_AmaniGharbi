05dba4aa2b83d0a17e188f94c9c4d2249053cd64
ameni <amanigharbi9@gmail.com>
2022-11-10 23:54:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/app_amanigharbi/SignIn1Activity.java
@@ -0,0 +1,20 @@
+package com.example.app_amanigharbi;
+
+import android.os.Bundle;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignIn1Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_1);
+
+
+    }
+}
